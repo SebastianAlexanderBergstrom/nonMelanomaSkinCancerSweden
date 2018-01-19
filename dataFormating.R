e068f864b2d4963f900dde8c056388290ce0cb6b
SebastianAlexanderBergstrom <30357348+SebastianAlexanderBergstrom@users.noreply.github.com>
2018-01-19 23:18:24 +0100

    Add files via upload

--- dataFormating.R
+++ dataFormating.R
@@ -2,37 +2,57 @@ formatDF<-function(dataFrame){
   # input: dataFrame is a data.frame object which we want to format in a certain way
   
   # output: returns a data.frame with the formatting we want
-  res <- dataFrame
-  
-  # Removes the last row which contains a string without data
-  res <- res[1:(dim(res)[1]-1),]
-  
+  res <- nm2[1:nrow(nm2)-1,]
   colnames(res)<-c("Year","County","Age","Men","Women")
   
-  res$Year <- as.numeric(as.character(res$Year))
-  years<-rep(res$Year,2)
+  # "Duplicate" the data frame, have to change the values
+  nonSex <- subset(res,select=-c(Men,Women))
+  nonSex[nrow(nonSex)+1:nrow(nonSex),] <- nonSex
   
-  lst<-c("0-4","5-9","10-14","15-19","20-24","25-29","30-34","35-39","40-44","45-49",
-         "50-54","55-59","60-64","65-69","70-74","75-59","80-84","85+")
-  age <- rep(factor(lst,levels=lst),2024)
-  counties<-rep(res$County,2)
+  # Convert "Year" to numeric
+  nonSex$Year <- as.numeric(nonSex$Year)
   
-  # County names are translated to English and formated. We had some problems with Skane, Orebro,
-  # Uppsala and Vastra Gotaland, the commands below fix those issues.
-  counties <- gsub("s län","",counties)
-  counties <- gsub("län","",counties)
-  counties <- gsub(" ","",counties)
-  counties <- gsub("Riket","Sweden",counties)
+  ageGroupFormat <- function(ageString){
+    if(nchar(ageString) == 2){
+      return(paste(substr(ageString,1,1),"-",substr(ageString,2,2),sep=""))
+    }
+    else{
+      return(paste(substr(ageString,1,2),"-",substr(ageString,3,5),sep=""))
+    }
+  }
   
-  # Remove commas and blank spaces for numerical valuess
-  cases <- unlist(list(res$Men, res$Women))
-  cases <- gsub(" ","",cases)
-  cases <- gsub(",",".",cases)
-  cases <- as.numeric(cases)
+  nonSex$Age <- as.character(res$Age)
+  nonSex$Age <- as.factor(sapply(nonSex$Age,ageGroupFormat))
   
-  n<-length(res$Men)
-  sexInd <- c(rep("Men",n),rep("Women",n))
+  countyFormat <- function(countyString){
+    if(countyString == "Riket"){
+      return("Sweden")
+    }
+    countyName <- strsplit(countyString,split=" ")[[1]][1]
+    # Remove the last "s", e.g. "Hallands" becomes "Halland"
+    if(substr(countyName,nchar(countyName),nchar(countyName)) == "s"){
+      countyName <- substr(countyName,1,nchar(countyName)-1)
+    }
+    return(countyName[[1]])
+  }
   
-  res<-data.frame(years,age,counties,cases,sexInd)
-  return(res)
+  nonSex$County <- as.character(nonSex$County)
+  nonSex$County <- as.factor(sapply(nonSex$County,countyFormat))
+  
+  # Format the response variables
+  response <- c(res$Men,res$Women)
+  response <- gsub(",",".",response)
+  response <- gsub(" ","",response)
+  response <- as.numeric(response)
+  
+  # Indicator for sex
+  #men <- rep(1,nrow(res))
+  #women <- rep(0,nrow(res))
+  #sexInd <- c(men,women)
+  
+  # Can also use this
+  sexInd <- c(rep("Men",length(res$Men)),rep("Women",length(res$Women)))
+  
+  result <- cbind(nonSex,sexInd,response)
+  return(result)
 }
